c62063ada25ae859f399d0d49f9b4a6b76b783f2
ClementMUR <clems.mur@gmail.com>
2025-02-27 15:33:48 +0100

    Added g versus r plot

--- notebooks/magcvs_library/functions.py
+++ notebooks/magcvs_library/functions.py
@@ -70,3 +70,60 @@ def corner_plot(df: pd.DataFrame, df2: pd.DataFrame = None, data_labels: list[st
     fig.tight_layout()
 
     return
+
+
+def modified_corner_plot(dfx: pd.DataFrame, dfy: pd.DataFrame, df2x: pd.DataFrame, df2y: pd.DataFrame, data_labels: list[str] | None = None) -> None:
+    """Modified version of the corner_plot function to plot the data from two different filters. Data from filter x is plotted on the x axis and data from filter y is plotted on the y axis.  
+    dfx, dfy, df2x and df2y should have the same columns.
+
+    Args:
+        dfx (pd.DataFrame): The positive class data from the g filter.
+        dfy (pd.DataFrame): The positive class data from the r filter.
+        df2x (pd.DataFrame): The negative class data from the g filter.
+        df2y (pd.DataFrame): The negative class data from the r filter.
+        data_labels (list[str] | None, optional): The labels for the positive and negative class data. Defaults to None.
+    """
+
+    n = len(dfx.columns) # Dimension of the corner plot
+
+    fig = plt.figure(figsize=(n*2, n*2))
+    
+    # Plotting diagonal:
+    index = 1
+    for feature_name in dfx.columns:
+        plt.subplot(n, n, index)
+        plt.scatter(dfx[feature_name], dfy[feature_name], s=1, alpha=0.1)
+        plt.scatter(df2x[feature_name], df2y[feature_name], s=1, alpha=0.1)
+        if index == n*n:
+            plt.xlabel(feature_name)
+        else:
+            plt.xticks([])
+        if index != 1:
+            plt.yticks([])
+        else:
+            plt.ylabel(feature_name)
+        index += n + 1
+
+    # Plotting scatter plots:
+    feature_pairs = tuple(combinations(dfx.columns, 2))
+    pair = 0
+    for i in range(1, n):
+        for j in range(i, n):
+            plt.subplot(n, n, j*n + i)
+            plt.scatter(dfx[feature_pairs[pair][0]], dfy[feature_pairs[pair][1]], s=1, alpha=0.1)
+            plt.scatter(df2x[feature_pairs[pair][0]], df2y[feature_pairs[pair][1]], s=1, alpha=0.1)
+            if i == 1:
+                plt.ylabel(feature_pairs[pair][1])
+            else:
+                plt.yticks([])
+            if j == n - 1:
+                plt.xlabel(feature_pairs[pair][0])
+            else:
+                plt.xticks([])
+            pair += 1
+
+    if data_labels is not None:
+        fig.legend(data_labels, loc='center right')
+    fig.tight_layout()
+
+    return
